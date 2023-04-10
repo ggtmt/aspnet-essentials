7a023d9c407ac735da37f51e4f1b76e086d7dd4c
Matheus <matheus.f.paula7@aluno.senai.br>
2023-04-10 17:47:01 -0300

    aula1 de fundamentos

---
+++ Models/Tarefa.cs
@@ -0,0 +1,21 @@
+namespace GerenciaTarefa.Models;
+//conjunto de classes
+public class Tarefa{
+
+//classe e um conjunto de objetos
+//propiedades
+public int id { get; set; }
+
+
+public string? Cabecalho { get; set; }
+
+public string? Titulo { get; set; }
+
+public string? Descricao {get; set;}
+
+public bool TaCompleta {get; set;}
+
+
+//funcionalidades
+
+}
